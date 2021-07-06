819a5fbcaf0d9c011545ae83e2033262ea9cbe4c
joalereyesu <61555348+joalereyesu@users.noreply.github.com>
2021-07-06 14:09:12 -0600

    cambios

--- Functions.py
+++ Functions.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from datetime import date, datetime
 import time
+import random
 
 def makeX(horas, time):
     tiempo = []
@@ -13,7 +14,50 @@ def makeX(horas, time):
         tiempo.append(result)
     return tiempo
 
-def checkDates(db, di, df):
-    #datei = datetime.strptime(di, '%Y-%m-%d')
-    #datef = datetime.strptime(df, '%Y-%m-%d')
-    print(db.date_range(start = di, end = df))
+def getPoints(db, hora_med, di, df):
+    horas = []
+    rand_horas = []
+    glucosa = []
+    rand_glucosa = []
+    condicion = []
+    rand_condicion = []
+    puntosx = []
+    datei = datetime.strptime(di, '%Y-%m-%d')
+    datef = datetime.strptime(df, '%Y-%m-%d')
+    for index, row in db.iterrows():
+        if row['Fecha'] == datei:
+            inicio = index
+        if row['Fecha'] == datef:
+            final = index
+
+    for element in range(inicio, final):
+        horas.append(str(db.loc[element, 'Hora']))
+        glucosa.append(int(db.loc[element, 'mg/dL']))
+        condicion.append(str(db.loc[element, 'Condición']))
+
+    if len(horas) <= 10:
+        puntosx = makeX(horas, hora_med)
+        return puntosx, horas, glucosa, condicion
+    else: 
+        while (len(rand_horas)<10):
+            rand_num = random.randint(0, (len(horas)-1))
+            element_h = horas[rand_num]
+            element_g = glucosa[rand_num]
+            element_c = condicion[rand_num]
+            if element not in rand_horas:
+                rand_horas.append(element_h)
+                rand_glucosa.append(element_g)
+                rand_condicion.append(element_c)
+        puntosx = makeX(rand_horas, hora_med)
+        return puntosx, rand_horas, rand_glucosa, rand_condicion
+
+def RazonCambio(x, y):
+    n = len(x)
+    dx = []
+    dx[0] = (y[1] - y[0])/(x[1] - x[0])
+    i = 2
+    while i != (n-1):
+        dx[i] = (y[i+1] - y[i-1])/(x[i+1] - x[i-1])
+        i = i + 1
+    dx[n] =  (y[n] - y[n-1])/(x[n] - x[n-1])
+    return dx
--- MainFINAL.py
+++ MainFINAL.py
@@ -2,49 +2,34 @@ import pandas as pd
 import os
 from datetime import datetime
 import Functions
-import random
 
 db = pd.read_excel('Glucosa-Project.xlsx')
 database = db.dropna()
 
+opcion = 0
 hora_med = input("Ingrese la hora de su medicamento (HH:MM): ")
-print("MENU\n\n 1. Rango de Analisis\n 2. Graficas\n 3. Tabla de metabolizacion de Glucosa\n 4. Aceleracion metabolica de la glucos \n 5. Glucosa Promedio\n 6. Glucosa-meta\n 7. Tendencia\n 8. Resumen Estadistico\n")
-opcion = int(input("Ingrese una opcion: "))
-horas = []
-rand_horas = []
+decimal_time = []
+time = []
 glucosa = []
-rand_glucosa = []
-rand_condicion = []
+condicion = []
 
-if (opcion == 1):
-    fecha_inicial = input("Ingrese la fecha de inicio de la muestra (yyyy-mm-dd): ")
-    fecha_final = input("Ingrese la fecha final de la muestra (yyyy-mm-dd): ")
-    datei = datetime.strptime(fecha_inicial, '%Y-%m-%d')
-    datef = datetime.strptime(fecha_final, '%Y-%m-%d')
-    for index, row in database.iterrows():
-        if row['Fecha'] == datei:
-            inicio = index
-        if row['Fecha'] == datef:
-            final = index
-
-    for element in range(inicio, final):
-        horas.append(str(db.loc[element, 'Hora']))
-        glucosa.append(str(db.loc[element, 'mg/dL']))
-        condicion.append(str(db.loc[element, 'Condición']))
-
-    if len(horas) <= 10:
-        puntosx = Functions.makeX(horas, hora_med)
-    else: 
-        while (len(rand_horas)<10):
-            rand_num = random.randint(0, (len(horas)-1))
-            element_h = horas[rand_num]
-            element_g = glucosa[rand_num]
-            element_c = condicion[rand_num]
-            if element not in rand_horas:
-                rand_horas.append(element_h)
-                rand_glucosa.append(element_g)
-                rand_condicion.append(element_c)
-        puntosx = Functions.makeX(rand_horas, hora_med)
-
+while (opcion != 9):
+    print("MENU\n\n 1. Rango de Analisis\n 2. Graficas\n 3. Tabla de metabolizacion de Glucosa\n 4. Aceleracion metabolica de la glucos \n 5. Glucosa Promedio\n 6. Glucosa-meta\n 7. Tendencia\n 8. Resumen Estadistico\n")
+    opcion = int(input("Ingrese una opcion: "))
+    if (opcion == 1):
+        fecha_inicial = input("Ingrese la fecha de inicio de la muestra (yyyy-mm-dd): ")
+        fecha_final = input("Ingrese la fecha final de la muestra (yyyy-mm-dd): ")
+        (decimal_time, time, glucosa, condicion) = Functions.getPoints(database, hora_med, fecha_inicial, fecha_final)
+            
+    if opcion == 3:
+        print("TABLA DE METABOLIZACION DE GLUCOSA\n")
+        razon_cambio = Functions.RazonCambio(decimal_time, glucosa)
+        i = 1
+        print("No.      Tiempo      Glucosa      Razon de Cambio       Condicion\n")
+        print("---------------------------------------------------------------------")
+        while i != (len(razon_cambio) + 1):
+            print(f"{i}      {time[i]}      {glucosa[i]}      {razon_cambio[i]}      {condicion[i]}")
+            i = i + 1
+        print("---------------------------------------------------------------------")
         
 
